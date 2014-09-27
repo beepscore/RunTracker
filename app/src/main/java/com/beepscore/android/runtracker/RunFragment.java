85b27ec6c69ed5963c438fc92faa25824f8917f3
beepscore <support@beepscore.com>
2014-09-27 16:29:40 -0700

    Fix broken build. In RunActivity delete all template methods and add createFragment(). Add RunFragment.java.

---
+++ app/src/main/java/com/beepscore/android/runtracker/RunFragment.java
@@ -0,0 +1,61 @@
+package com.beepscore.android.runtracker;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class RunFragment extends Fragment {
+    private Button mStartButton;
+    private Button mStopButton;
+    private TextView mStartedTextView;
+    private TextView mLatitudeTextView;
+    private TextView mLongitudeTextView;
+    private TextView mAltitudeTextView;
+    private TextView mDurationTextView;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment RunFragment.
+     */
+    public static RunFragment newInstance() {
+        RunFragment fragment = new RunFragment();
+        return fragment;
+    }
+
+    public RunFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_run, container, false);
+
+        mStartedTextView = (TextView)view.findViewById(R.id.run_startedTextView);
+        mLatitudeTextView = (TextView)view.findViewById(R.id.run_latitudeTextView);
+        mLongitudeTextView = (TextView)view.findViewById(R.id.run_longitudeTextView);
+        mAltitudeTextView = (TextView)view.findViewById(R.id.run_altitudeTextView);
+        mDurationTextView = (TextView)view.findViewById(R.id.run_durationTextView);
+
+        mStartButton = (Button)view.findViewById(R.id.run_startButton);
+        mStopButton = (Button)view.findViewById(R.id.run_stopButton);
+
+        return view;
+    }
+
+}
