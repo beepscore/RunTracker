9743bdffeb7d97f07ba902ae9f46f3379d46f134
beepscore <support@beepscore.com>
2014-09-28 13:58:30 -0700

    In TrackingLocationReceiver onLocationReceived log latitude and longitude.
    Book notes that you can start app in Android Studio, look at logcat output to see updates, stop app on phone, and observe logcat is still getting location updates.

--- app/src/main/java/com/beepscore/android/runtracker/TrackingLocationReceiver.java
+++ app/src/main/java/com/beepscore/android/runtracker/TrackingLocationReceiver.java
@@ -2,6 +2,7 @@ package com.beepscore.android.runtracker;
 
 import android.content.Context;
 import android.location.Location;
+import android.util.Log;
 
 /**
  * Created by stevebaker on 9/28/14.
@@ -10,7 +11,10 @@ public class TrackingLocationReceiver extends LocationReceiver {
 
     @Override
     protected void onLocationReceived(Context context, Location location) {
+        String TAG = "TrackingLocationReceiver";
         RunManager.get(context).insertLocation(location);
+        Log.d(TAG, "latitude " + Double.toString(location.getLatitude()));
+        Log.d(TAG, "longitude " + Double.toString(location.getLongitude()));
     }
 
 }
