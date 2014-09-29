5b5861bbfcd00d4437866928d66cbfe64259a100
beepscore <support@beepscore.com>
2014-09-29 10:10:58 -0700

    Add class DataLoader

---
+++ app/src/main/java/com/beepscore/android/runtracker/DataLoader.java
@@ -0,0 +1,38 @@
+package com.beepscore.android.runtracker;
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+
+/**
+ * Created by stevebaker on 9/29/14.
+ * Generic loader for arbitrary data.
+ * Does not use a Cursor.
+ * Subclasses will implement loadInBackground
+ */
+public abstract class DataLoader<D> extends AsyncTaskLoader<D> {
+
+    // D is a generic data type
+    private D mData;
+
+    public DataLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        if (mData != null) {
+            deliverResult(mData);
+        } else {
+            forceLoad();
+        }
+    }
+
+    @Override
+    public void deliverResult(D data) {
+        mData = data;
+        if (isStarted()) {
+            super.deliverResult(data);
+        }
+    }
+
+}
