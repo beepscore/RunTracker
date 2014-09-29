1d11c6507b45dc11fed032f055564a17a4f0d34d
beepscore <support@beepscore.com>
2014-09-28 19:55:54 -0700

    In RunListFragment.java add class RunCursorAdapter. In RunListFragment onCreate create a RunCursorAdapter and use it as the list adapter.

--- app/src/main/java/com/beepscore/android/runtracker/RunListFragment.java
+++ app/src/main/java/com/beepscore/android/runtracker/RunListFragment.java
@@ -1,7 +1,14 @@
 package com.beepscore.android.runtracker;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.support.v4.app.ListFragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 import com.beepscore.android.runtracker.RunDatabaseHelper.RunCursor;
 
@@ -20,12 +27,45 @@ public class RunListFragment extends ListFragment {
         // TODO: Instead use a Loader to load in background
         // Query the list of runs
         mCursor = RunManager.get(getActivity()).queryRuns();
+
+        // Create an adapter to point at this cursor
+        RunCursorAdapter adapter = new RunCursorAdapter(getActivity(), mCursor);
+        setListAdapter(adapter);
     }
 
     @Override
     public void onDestroy() {
         mCursor.close();
         super.onDestroy();
+    }
+
+    private static class RunCursorAdapter extends CursorAdapter {
+
+        private RunCursor mRunCursor;
+
+        private RunCursorAdapter(Context context, RunCursor cursor) {
+            super(context, cursor, 0);
+            mRunCursor = cursor;
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            // Use a layout inflater to get a row view
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            return inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            // Get the run for the current row
+            Run run = mRunCursor.getRun();
+
+            // Set up the start date text view
+            TextView startDateTextView = (TextView)view;
+
+            String cellText = context.getString(R.string.cell_text, run.getStartDate());
+            startDateTextView.setText(cellText);
+        }
     }
 
 }
