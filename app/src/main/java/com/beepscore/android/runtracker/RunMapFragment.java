f00407e5417ee56b7588cc1d24298689a90052c9
beepscore <support@beepscore.com>
2014-12-02 12:06:02 -0800

    In RunMapFragment use LoaderManager to load locations in background. Implement LoaderManager.LoaderCallbacks<Cursor>

--- app/src/main/java/com/beepscore/android/runtracker/RunMapFragment.java
+++ app/src/main/java/com/beepscore/android/runtracker/RunMapFragment.java
@@ -1,5 +1,8 @@
 package com.beepscore.android.runtracker;
 
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +14,14 @@ import com.google.android.gms.maps.MapFragment;
 /**
  * Created by stevebaker on 12/1/14.
  */
-public class RunMapFragment extends MapFragment {
+public class RunMapFragment extends MapFragment
+        implements LoaderManager.LoaderCallbacks<Cursor> {
+
     private static final String ARG_RUN_ID = "RUN_ID";
+    private static final int LOAD_LOCATIONS = 0;
 
     private GoogleMap mGoogleMap;
+    private LocationCursor mLocationCursor;
 
     public static RunMapFragment newInstance(long runId) {
         Bundle args = new Bundle();
@@ -22,6 +29,21 @@ public class RunMapFragment extends MapFragment {
         RunMapFragment runMapFragment = new RunMapFragment();
         runMapFragment.setArguments(args);
         return runMapFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Check for a Run ID as an argument, and find the run
+        Bundle args = getArguments();
+        if (args != null) {
+            long runId = args.getLong(ARG_RUN_ID, -1);
+            if (runId != -1) {
+                LoaderManager loaderManager = getLoaderManager();
+                loaderManager.initLoader(LOAD_LOCATIONS, args, this);
+            }
+        }
     }
 
     @Override
@@ -35,6 +57,26 @@ public class RunMapFragment extends MapFragment {
         mGoogleMap.setMyLocationEnabled(true);
 
         return view;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        long runId = args.getLong(ARG_RUN_ID, -1);
+        return new LocationListCursorLoader(getActivity(), runId);
+    }
+
+    // LoaderCallbacks
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mLocationCursor = (LocationCursor)cursor;
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Stop using the data
+        mLocationCursor.close();
+        mLocationCursor = null;
     }
 
 }
